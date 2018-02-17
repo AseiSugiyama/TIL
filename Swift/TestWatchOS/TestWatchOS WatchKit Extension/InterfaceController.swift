d7f2d8a3fbf4c938c8f775396d1382275c40c9b6
Ryuichirou <gokenya@gmail.com>
2018-02-17 13:57:23 +0900

    init

---
+++ Swift/TestWatchOS/TestWatchOS WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TestWatchOS WatchKit Extension
+//
+//  Created by Asei on 2018/02/17.
+//  Copyright © 2018年 gokennya. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
