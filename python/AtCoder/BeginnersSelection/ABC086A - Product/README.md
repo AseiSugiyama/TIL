daf87937d54440016c78e10518812c4a40fb7fb9
Ryuichirou <gokenya@gmail.com>
2018-04-21 12:08:21 +0900

    Add : [ABC086A - Product](https://beta.atcoder.jp/contests/abs/tasks/abc086_a)

---
+++ python/AtCoder/BeginnersSelection/ABC086A - Product/README.md
@@ -0,0 +1,1 @@
+From [ABC086A - Product](https://beta.atcoder.jp/contests/abs/tasks/abc086_a)
---
+++ python/AtCoder/BeginnersSelection/ABC086A - Product/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+a, b = map(int, input().split())
+
+if(a % 2 == 0 or b % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
