257cb56d1b55ccfff119f0e88a22f0e97a30e711
Ryuichirou <gokenya@gmail.com>
2018-06-02 13:29:26 +0900

    Step 6: Navigate to a new screen

--- Dart/firstflutterapp/lib/main.dart
+++ Dart/firstflutterapp/lib/main.dart
@@ -30,8 +30,43 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
         appBar: AppBar(
           title: Text('Startup Name Generator'),
+          actions: <Widget>[
+            IconButton(icon:Icon(Icons.list),onPressed: _pushSaved),
+          ],
         ),
-        body: _buildSuggestions());
+        body: _buildSuggestions()
+    );
+  }
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map( 
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            }
+          );
+          final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+          
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ), 
+            body: ListView(children: divided),
+          );
+        }
+      ),
+    );
   }
 
   Widget _buildSuggestions() {
