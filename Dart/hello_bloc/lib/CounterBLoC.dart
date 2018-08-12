08babb3b611f3f5b6ee4a22df54132226a678a79
Ryuichirou <gokenya@gmail.com>
2018-08-12 16:39:42 +0900

    Add : CounterBLoC

---
+++ Dart/hello_bloc/lib/CounterBLoC.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+
+class CounterBloc{
+  StreamController counterAdditionController = StreamController();
+  Sink get counterAddition => counterAdditionController.sink;
+  BehaviorSubject<int> _count = BehaviorSubject<int>(seedValue: 0);
+  Stream<int> get countString => _count.stream;
+
+  CounterBloc(){
+    counterAdditionController.stream.listen( (addition) {
+      _count.add(_count.value + 1);
+    });
+  }
+}  
+
